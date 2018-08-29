cced4ecd7ba26da4b4091c611c0a623c6b4b5cb6
chandank <chandank@meditab.com>
2018-08-29 15:25:51 +0530

    Changes for person collection

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace MVCApp.Controllers
 {
     public class HomeController : Controller
     {
+        List<PersonalInfo> personalInforCollection = new List<Models.PersonalInfo>();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +27,36 @@ namespace MVCApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult PersonalInfo(int personId)
+        {
+            personalInforCollection.Add(new PersonalInfo() { PersonId = 1, Date = new DateTime(2015, 4, 12), FirstName = "Hari", LastName = "Sadu" });
+            personalInforCollection.Add(new PersonalInfo() { PersonId = 2, Date = new DateTime(2015, 4, 12), FirstName = "Hari2", LastName = "Sadu" });
+            personalInforCollection.Add(new PersonalInfo() { PersonId = 3, Date = new DateTime(2015, 4, 12), FirstName = "Hari3", LastName = "Sadu3" });
+
+            var person = personalInforCollection.FirstOrDefault(x => x.PersonId == personId);
+
+            return View(person);
+        }
+
+
+        [HttpPost]
+        public ActionResult PersonalInfo(PersonalInfo personalInfo)
+        {
+            personalInforCollection.Add(new PersonalInfo() { PersonId = 1, Date = new DateTime(2015, 4, 12), FirstName = "Hari", LastName = "Sadu" });
+            personalInforCollection.Add(new PersonalInfo() { PersonId = 2, Date = new DateTime(2015, 4, 12), FirstName = "Hari2", LastName = "Sadu" });
+            personalInforCollection.Add(new PersonalInfo() { PersonId = 3, Date = new DateTime(2015, 4, 12), FirstName = "Hari3", LastName = "Sadu3" });
+
+            if (personalInforCollection.Any(p=>p.PersonId == personalInfo.PersonId))
+            {
+               var person = personalInforCollection.FirstOrDefault(x => x.PersonId == personalInfo.PersonId);
+                personalInforCollection.Remove(person);
+                personalInforCollection.Add(personalInfo);
+            }
+
+            return View("personalInfoList",personalInforCollection);
         }
     }
 }
---
+++ MVCApp/Models/PersonalInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class PersonalInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Date { get; set; }
+        public int PersonId { get; set; }
+
+    }
+}
