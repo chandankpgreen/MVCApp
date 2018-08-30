c4c47cd0560cdc210a91ac18d8c3427a9369ad48
chandank <chandank@meditab.com>
2018-08-30 19:37:44 +0530

    Changes

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace MVCApp.Controllers
     public class HomeController : Controller
     {
         List<PersonalInfo> personalInforCollection = new List<Models.PersonalInfo>();
+        SelectList lastNames;
         public ActionResult Index()
         {
             return View();
@@ -33,8 +34,20 @@ namespace MVCApp.Controllers
         public ActionResult PersonalInfo(int personId)
         {
             personalInforCollection.Add(new PersonalInfo() { PersonId = 1, Date = new DateTime(2015, 4, 12), FirstName = "Hari", LastName = "Sadu" });
-            personalInforCollection.Add(new PersonalInfo() { PersonId = 2, Date = new DateTime(2015, 4, 12), FirstName = "Hari2", LastName = "Sadu" });
+            personalInforCollection.Add(new PersonalInfo() { PersonId = 2, Date = new DateTime(2015, 4, 12), FirstName = "Hari2", LastName = "Sadu2" });
             personalInforCollection.Add(new PersonalInfo() { PersonId = 3, Date = new DateTime(2015, 4, 12), FirstName = "Hari3", LastName = "Sadu3" });
+
+
+            var p = new List<SelectListItem>();
+            p.Add(new SelectListItem() { Text = "1", Value = "Sadu" });
+            p.Add(new SelectListItem() { Text = "2", Value = "Sadu2", Selected = true });
+            p.Add(new SelectListItem() { Text = "3", Value = "Sadu3" });
+
+            lastNames = new SelectList(p,"Value", "Text");
+
+            ViewBag.LastNames = lastNames;
+
+          
 
             var person = personalInforCollection.FirstOrDefault(x => x.PersonId == personId);
 
@@ -45,8 +58,14 @@ namespace MVCApp.Controllers
         [HttpPost]
         public ActionResult PersonalInfo(PersonalInfo personalInfo)
         {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("LastName", "FirstName cannot be null or empty");
+                return View(personalInfo);
+            }
+            
             personalInforCollection.Add(new PersonalInfo() { PersonId = 1, Date = new DateTime(2015, 4, 12), FirstName = "Hari", LastName = "Sadu" });
-            personalInforCollection.Add(new PersonalInfo() { PersonId = 2, Date = new DateTime(2015, 4, 12), FirstName = "Hari2", LastName = "Sadu" });
+            personalInforCollection.Add(new PersonalInfo() { PersonId = 2, Date = new DateTime(2015, 4, 12), FirstName = "Hari2", LastName = "Sadu2" });
             personalInforCollection.Add(new PersonalInfo() { PersonId = 3, Date = new DateTime(2015, 4, 12), FirstName = "Hari3", LastName = "Sadu3" });
 
             if (personalInforCollection.Any(p=>p.PersonId == personalInfo.PersonId))
--- MVCApp/Models/PersonalInfo.cs
+++ MVCApp/Models/PersonalInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,21 @@ namespace MVCApp.Models
 {
     public class PersonalInfo
     {
+        [Required]
         public string FirstName { get; set; }
+        [EmailAddress]
+        [ReadOnly(true)]
+        public string Email { get; set; }
         public string LastName { get; set; }
         public DateTime Date { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:c}")]
         public int PersonId { get; set; }
+
+    }
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+    public class MaxValidationAttribute : ValidationAttribute
+    {
 
     }
 }
